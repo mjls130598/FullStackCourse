556c1639653667a5e5911834b119cf75a99a38ef
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2025-05-02 17:06:06 +0200

    Solution exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,9 +15,9 @@ const Part = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <Part part={props.content[0].part} exercises={props.content[0].exercises}/>
-      <Part part={props.content[1].part} exercises={props.content[1].exercises}/>
-      <Part part={props.content[2].part} exercises={props.content[2].exercises}/>
+      <Part part={props.content[0].name} exercises={props.content[0].exercises}/>
+      <Part part={props.content[1].name} exercises={props.content[1].exercises}/>
+      <Part part={props.content[2].name} exercises={props.content[2].exercises}/>
     </div>
   )
 }
@@ -30,24 +30,28 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const content = [
-    {"part": part1, "exercises": exercises1},
-    {"part": part2, "exercises": exercises2},
-    {"part": part3, "exercises": exercises3}
+    part1, part2, part3
   ]
 
   return (
     <div>
       <Header course={course}/>
       <Content content={content}/>
-      <Footer number={exercises1 + exercises2 + exercises3}/>
+      <Footer number={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
