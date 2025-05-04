805645aff0c311ad7c78882db3cd5827c83bf5a0
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2025-05-04 19:59:37 +0200

    Example server data

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,57 +1,56 @@
-import { useState } from 'react'
-
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-
-  const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min)) + min)
-
-  const handleClick = () =>{
-    let random = getRandomNumber(0, anecdotes.length)
-    setSelected(random)
-  }
-
-  const voteAnecdote = () => {
-    setVotes(prevVotes => {
-      const newVotes = [...prevVotes]
-      newVotes[selected] += 1
-      return newVotes
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
     })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  const getMostVote = () => (votes.indexOf(Math.max(...votes)))
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
+      <h1>Notes</h1>
       <div>
-        <h1>Anecdote of the day</h1>
-        <p>{anecdotes[selected]}</p>
-        <p>has {votes[selected]} votes</p>
-        <Button text="next anecdote" handleClick={handleClick}/>
-        <Button text="vote" handleClick={voteAnecdote}/>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
       </div>
-      <div>
-        <h1>Anecdote with most votes</h1>
-        <p>{anecdotes[getMostVote()]}</p>
-        <p>has {votes[getMostVote()]} votes</p>
-      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
