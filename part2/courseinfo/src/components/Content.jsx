7841c4362d8dd7371f2c659d235821188cf8589d
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2025-05-04 16:58:30 +0200

    Solution of exercise 2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -4,6 +4,11 @@ const Content = ({parts}) =>{
     return(
         <div>
             {parts.map(part => <Part key={part.id} part={part}/>)}
+            <p>
+                <strong>
+                    total of {parts.reduce((sum, part) => {return(sum + part.exercises)}, 0)} exercises
+                </strong>
+            </p>
         </div>
     )
 }
