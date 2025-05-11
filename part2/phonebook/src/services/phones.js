68a79e565c25d0b547bccdd10653e7d72fae502e
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2025-05-11 19:08:23 +0200

    Solution of exercise 3.11

--- part2/phonebook/src/services/phones.js
+++ part2/phonebook/src/services/phones.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://phonebook-y9ep.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     return axios.get(baseUrl)
